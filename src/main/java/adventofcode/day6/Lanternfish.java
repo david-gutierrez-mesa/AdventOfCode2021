ce39d446174d63cecf410a0bc31f50ec30a4113a
David Gutierrez <david.gutierrez@liferay.com>
2021-12-13 00:28:41 +0100

    Rename variable

--- src/main/java/adventofcode/day6/Lanternfish.java
+++ src/main/java/adventofcode/day6/Lanternfish.java
@@ -10,25 +10,25 @@ public class Lanternfish {
     private Lanternfish() {
     }
 
-    public static Long calculateNumber(String path, int days){
+    public static Long calculateNumber(String path, int days) {
         FileReaderClass fileReader = new FileReaderClass(path);
         List<Integer> lanternfishes = Arrays.stream(fileReader.readStrings().get(0).trim().split(",")).map(Integer::parseInt).collect(Collectors.toList());
 
         long[] daysToDuplicate = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-        for (Integer lanternfish : lanternfishes){
+        for (Integer lanternfish : lanternfishes) {
             daysToDuplicate[lanternfish] += 1;
         }
 
         long result = 0;
 
         for (int k = 0; k < days; k++) {
-            long newfishes = daysToDuplicate[0];
+            long newFishes = daysToDuplicate[0];
             System.arraycopy(daysToDuplicate, 1, daysToDuplicate, 0, daysToDuplicate.length - 1);
-            daysToDuplicate[6] += newfishes;
-            daysToDuplicate[daysToDuplicate.length-1] = newfishes;
+            daysToDuplicate[6] += newFishes;
+            daysToDuplicate[daysToDuplicate.length - 1] = newFishes;
         }
 
-        for (long i :daysToDuplicate) {
+        for (long i : daysToDuplicate) {
             result += i;
         }
         return result;
