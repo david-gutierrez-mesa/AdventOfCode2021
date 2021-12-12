4d1db78dc127f5985dcf88c0da5de77092e37def
David Gutierrez <david.gutierrez@liferay.com>
2021-12-13 00:27:30 +0100

    Day 6

---
+++ src/main/java/adventofcode/day6/Lanternfish.java
@@ -0,0 +1,36 @@
+package adventofcode.day6;
+
+import adventofcode.common.FileReaderClass;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lanternfish {
+    private Lanternfish() {
+    }
+
+    public static Long calculateNumber(String path, int days){
+        FileReaderClass fileReader = new FileReaderClass(path);
+        List<Integer> lanternfishes = Arrays.stream(fileReader.readStrings().get(0).trim().split(",")).map(Integer::parseInt).collect(Collectors.toList());
+
+        long[] daysToDuplicate = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (Integer lanternfish : lanternfishes){
+            daysToDuplicate[lanternfish] += 1;
+        }
+
+        long result = 0;
+
+        for (int k = 0; k < days; k++) {
+            long newfishes = daysToDuplicate[0];
+            System.arraycopy(daysToDuplicate, 1, daysToDuplicate, 0, daysToDuplicate.length - 1);
+            daysToDuplicate[6] += newfishes;
+            daysToDuplicate[daysToDuplicate.length-1] = newfishes;
+        }
+
+        for (long i :daysToDuplicate) {
+            result += i;
+        }
+        return result;
+    }
+}
